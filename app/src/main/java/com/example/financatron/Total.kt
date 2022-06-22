def9d0f8d584abfa3318f30a0f5382ebb36f6d17
David Siqueira <david@videnci.com.br>
2022-06-22 20:08:39 -0300

    recycler view working

--- app/src/main/java/com/example/financatron/Total.kt
+++ app/src/main/java/com/example/financatron/Total.kt
@@ -3,15 +3,42 @@ package com.example.financatron
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.financatron.product.ProductAdapter
 
 class Total : AppCompatActivity() {
 
-    private lateinit var userName : String
+    lateinit var recyclerView: RecyclerView
+    lateinit var userName: String
+    lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_total)
+        userName = intent.getStringExtra("Username").toString()
+
+        recyclerView = findViewById(R.id.recyclerView)
+        productAdapter = ProductAdapter(userName, clickListener = object : OnClickedListener {
+            override fun onItemClicked(view: View, position: Int) {
+//                val user = DataModel.instance.users[position]
+//                user.name = "Nome Deletado!"
+//                DataModel.instance.updateUser(user)
+//                productAdapter.notifyItemChanged(position)
+            }
+
+            override fun onItemLongClicked(view: View, position: Int) {
+//                DataModel.instance.removeUser(
+//                    DataModel.instance.users[position]
+//                )
+//                productAdapter.notifyItemRemoved(position)
+            }
+
+        })
+        recyclerView.adapter = productAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         userName = intent.getStringExtra("Username").toString()
 
